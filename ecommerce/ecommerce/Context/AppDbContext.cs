44518242591daffb001b89bcdb055ee19b1f8048
unknown <amrelsaid52@gmail.com>
2023-04-24 10:28:17 +0200

    product details view

--- ecommerce/ecommerce/Context/AppDbContext.cs
+++ ecommerce/ecommerce/Context/AppDbContext.cs
@@ -40,7 +40,7 @@ public class AppDbContext : IdentityDbContext<ApplicationUser>
 	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 	{
 
-		optionsBuilder.UseSqlServer("Server=.; Database=Ecommerce; Trusted_Connection=true; Encrypt=false; MultipleActiveResultSets=True");
+		optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS; Database=Ecommerce; Trusted_Connection=true; Encrypt=false; MultipleActiveResultSets=True");
 
 	}
 
